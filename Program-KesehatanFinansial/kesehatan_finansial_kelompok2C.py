7649d0b56ebbb29a7f6adab8b61ef05fa39c825f
igochaniago <65951124+igochaniago@users.noreply.github.com>
2020-06-02 16:12:17 +0700

    Add files via upload

---
+++ Program-KesehatanFinansial/kesehatan_finansial_kelompok2C.py
@@ -0,0 +1,32 @@
+#===================================================================
+#KesehatanFinansial v1.0
+#===================================================================
+#
+#Dibuat oleh Kelompok 2 Kelas C
+#
+#Yustika Kusuma Putri            I0318096
+#Rudolf Sahat Marisi Marpaung    I0319091
+#Satrio Fachri Chaniago          I0319096
+#Yunia Nur Afrasaniy Afina       I0319111
+#Zahra Humaida Rahman            I0319112
+#Foncio Flormoy De Jesus Moreira I0319115
+#
+#Mahasiswa S1 Teknik Industri UNS
+#
+#Versi ini merupakan pengembangan awal
+#
+#List Fitur:
+#   +Penggunaan CSV module untuk menyimpan data input di file .csv
+#   +Penggunaan Datetime module sebagai sistem penanggalan
+#   +Penggunaan Time module agar mempermudah saat dijalankan langsung dari klik programnya
+#   +Penggunaan Operator module untuk mengurutkan data sesuai tanggal input dengan fungsi sorted()
+#   +Penggunaan Getpass module untuk proses Login yang lebih aman
+#   +Penggunaan system("cls") yang menghasilkan interface pada prompt lebih nyaman
+#   +Dapat dijalankan langsung dari file .py nya tanpa perlu starting prompt terlebih dahulu 
+#    dan program tidak autobreak sehingga memudahkan user apabila menggunakan 
+#    tanpa melalui starting prompt
+#
+#Saran dan Masukan untuk pengembangan sangat diperlukan
+#Terimakasih
+#
+#===================================================================
